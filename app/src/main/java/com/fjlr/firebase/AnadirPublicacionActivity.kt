08f6f7f99fbce21a5c8c06736d69f35d3161c4d1
fraaaan02 <franjlr2002@gmail.com>
2025-04-22 18:53:12 +0200

    Commit - actualizacion

--- app/src/main/java/com/fjlr/firebase/AnadirPublicacionActivity.kt
+++ app/src/main/java/com/fjlr/firebase/AnadirPublicacionActivity.kt
@@ -5,29 +5,16 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.fjlr.firebase.databinding.ActivityAppBinding
-import com.fjlr.firebase.utils.configurarBarraNavegacion
-import com.google.firebase.firestore.FirebaseFirestore
 
-class AppActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivityAppBinding
-    private val db = FirebaseFirestore.getInstance()
-
+class AnadirPublicacionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-
-        binding = ActivityAppBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
+        setContentView(R.layout.activity_anadir_publicacion)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        configurarBarraNavegacion(this, binding.barraNavegacion)
-
     }
 }
